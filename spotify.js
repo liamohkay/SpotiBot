96a9a31aeed8c0c7065501991f7dc53aac4926ae
liamohkay <liam.olsonkenny@gmail.com>
2021-03-29 22:57:30 -0700

    Set up first part of spotify oauth, get request to authorization enpoint

--- spotify.js
+++ spotify.js
@@ -1,1 +1,9 @@
-const reddit = requie('./reddit.js');
+// Libraries & depdencies
+const axios = require('axios');
+const reddit = require('./reddit.js');
+const config = require('./config.js');
+const spotify = require('spotify-web-api-js');
+
+axios.get(`https://accounts.spotify.com/authorize?client_id=${config.client}&response_type=code&redirect_uri=${config.redirect_uri}`)
+  .catch(err => console.log(err))
+  .then(resp => console.log(resp.status))
