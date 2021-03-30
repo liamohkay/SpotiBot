48bf112847ec440d2858b4920349c3a3c5cf4236
liamohkay <liam.olsonkenny@gmail.com>
2021-03-29 21:13:23 -0700

    Made removeYears fn

--- reddit.js
+++ reddit.js
@@ -8,13 +8,22 @@ const getTopPosts = (subreddit, callback, n=10, time='week') => {
     .catch(err => console.log(err))
 }
 
+// Removes tags (ft <artist>) and [feat <artist>] from title
 const removeFeatures = title => title.replace(/[([](FEAT|FT).*[\)\]]/i, '');
 
-getTopPosts('tech_house', resp => {
+// Removes tags with "extended" enclosed by () or []
+const removeExtended = title => title.replace(/[([](EXTENDED).*[\)\]]/i, '');
+
+// Removes years in format (yyyy) or [yyyy] from titles
+const removeYears = title => title.replace(/[([]\d{4}[\)\]]/i, '');
+
+
+getTopPosts('lofihouse', resp => {
   let arr = [];
   for (post in resp) {
     let title = resp[post].data.title;
     title = removeFeatures(title);
+    title = removeYears(title);
     arr.push(title);
   }
   console.log(arr);
