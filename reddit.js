f18f4d2cda7d3dd67b42cfb1c76ea338801e101b
liamohkay <liam.olsonkenny@gmail.com>
2021-03-29 20:31:09 -0700

    Added async await to getTopPosts

--- reddit.js
+++ reddit.js
@@ -2,10 +2,11 @@
 const axios = require('axios');
 
 // Retreives top posts from user specified subreddit. n & time default to 10 & 'week'
-const getTopPosts = (subreddit, n=10, time='week') => {
-  axios.get(`https://www.reddit.com/r/${subreddit}/top/.json?t=${time}`)
+const getTopPosts = async (subreddit, n=10, time='week') => {
+  return await axios.get(`https://www.reddit.com/r/${subreddit}/top/.json?t=${time}`)
     .then(resp => console.log(resp.data.data.children))
     .catch(err => console.log(err))
 }
 
-getTopPosts('house');
+let data = getTopPosts('house');
+console.log(data);
