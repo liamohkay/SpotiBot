980c512bf17b47d1e8e5aa9a66324f7012987b38
liamohkay <liam.olsonkenny@gmail.com>
2021-03-29 23:11:32 -0700

    Set up a little front end boiler plate

---
+++ ex-config.js
@@ -0,0 +1,6 @@
+// Spotify oauth params
+module.exports = {
+    redirect_uri: 'https://localhost:3001/callback/',
+    client: 'INSERT SPOTIFY APP CLIENT ID',
+    secret: 'INSERT SPOTIFY APP CLIENT SECRET'
+};
--- spotify.js
+++ spotify.js
@@ -4,6 +4,4 @@ const reddit = require('./reddit.js');
 const config = require('./config.js');
 const spotify = require('spotify-web-api-js');
 
-axios.get(`https://accounts.spotify.com/authorize?client_id=${config.client}&response_type=code&redirect_uri=${config.redirect_uri}`)
-  .catch(err => console.log(err))
-  .then(resp => console.log(resp.status))
+axios.get(`https://accounts.spotify.com/authorize?client_id=${config.client}&response_type=code&redirect_uri=${config.redirect_uri}`);
