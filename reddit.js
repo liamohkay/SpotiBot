1cab10b404315db04c7c5d86d984c18b4423867c
liamohkay <liam.olsonkenny@gmail.com>
2021-03-29 21:09:15 -0700

    Created removeFeatures

--- reddit.js
+++ reddit.js
@@ -2,11 +2,22 @@
 const axios = require('axios');
 
 // Retreives top posts from user specified subreddit. n & time default to 10 & 'week'
-const getTopPosts = async (subreddit, n=10, time='week') => {
-  return await axios.get(`https://www.reddit.com/r/${subreddit}/top/.json?t=${time}`)
-    .then(resp => console.log(resp.data.data.children))
+const getTopPosts = (subreddit, callback, n=10, time='week') => {
+  axios.get(`https://www.reddit.com/r/${subreddit}/top/.json?t=${time}`)
+    .then(resp => callback(resp.data.data.children))
     .catch(err => console.log(err))
 }
 
-let data = getTopPosts('house');
-console.log(data);
+const removeFeatures = title => title.replace(/[([](FEAT|FT).*[\)\]]/i, '');
+
+getTopPosts('tech_house', resp => {
+  let arr = [];
+  for (post in resp) {
+    let title = resp[post].data.title;
+    title = removeFeatures(title);
+    arr.push(title);
+  }
+  console.log(arr);
+  // let test = resp['0'].data.title;
+  // console.log(test);
+});
