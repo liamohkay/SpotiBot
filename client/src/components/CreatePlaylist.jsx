c9475794998e1902baba6a1e8d57a2a587e2278e
liamohkay <liam.olsonkenny@gmail.com>
2021-03-31 13:00:48 -0700

    Added playlist info to persistence

--- client/src/components/CreatePlaylist.jsx
+++ client/src/components/CreatePlaylist.jsx
@@ -4,7 +4,10 @@ const SpotifyAPI = new Spotify();
 
 const CreatePlaylist = ({ token, userID, data, setData, setSelected }) => {
   const [clicked, setClicked] = useState(false);
-  const [newPlaylist, setNewPlaylist] = useState({});
+  const [newPlaylist, setNewPlaylist] = useState({
+    name: '',
+    description: '',
+  });
 
   // Tracks + stores user input for new playlist name + description
   const handleChange = (e) => {
@@ -18,7 +21,31 @@ const CreatePlaylist = ({ token, userID, data, setData, setSelected }) => {
   const handleSave = (e) => {
     SpotifyAPI.createPlaylist(userID, newPlaylist)
       .catch(err => alert(`Failed to create your playlist ${newPlaylist.name}`))
-      .then(() => alert(`Created your playlist ${newPlaylist.name}`))
+      .then(() => {
+        SpotifyAPI.getUserPlaylists(userID)
+        .catch(err => console.log(err))
+        .then(resp => {
+          // Map through playlist to find the one we just created
+          resp.items.map(item => {
+            if (
+              item.name === newPlaylist.name &&
+              item.description === newPlaylist.description
+            ) {
+                let newData = data;
+                let playlistInfo = {
+                  id: item.id,
+                  subreddits: []
+                };
+                newData[newPlaylist.name] = playlistInfo;
+                setData(newData);
+                setSelected(newPlaylist.name);
+                console.log(data);
+                alert(`Created your playlist ${newPlaylist.name}`);
+            }
+          });
+        })
+      });
+
     setClicked(false);
   }
 
@@ -26,7 +53,6 @@ const CreatePlaylist = ({ token, userID, data, setData, setSelected }) => {
     <div id="create-playlist-container">
       <form>
         <button id="create-playlist-btn" onClick={() => setClicked(true)}>Create New Playlist</button>
-        { JSON.stringify(newPlaylist) }
         { !clicked ? null : (
           <div id="save-playlist-container">
             <h3>Enter A Playlist Name</h3>
