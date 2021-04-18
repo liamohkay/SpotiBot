d9405c5a1a71de5d2d963f7704d9787d2915c4c5
liamohkay <liam.olsonkenny@gmail.com>
2021-04-18 13:52:30 -0700

    Created firebase config for user auth + firestore

---
+++ client/src/firebase/fbConfig-ex.js
@@ -0,0 +1,13 @@
+// import firebase from 'firebase/app';
+// import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+export default firebaseConfig;
---
+++ client/src/firebase/firebase.js
@@ -0,0 +1,19 @@
+// Libraries + dependencies
+import firebaseConfig from './fbConfig.js';
+import firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/auth';
+
+// Initialize firebase app
+const fb = firebase.intializeApp({
+  apiKey: firebaseConfig.apiKey,
+  authDomain: firebaseConfig.authDomain,
+  projectId: firebaseConfig.projectId,
+  storageBucket: firebaseConfig.storageBucket,
+  messagingSenderId: firebaseConfig.messagingSenderId,
+  appId: firebaseConfig.appId
+});
+
+export const storage =  fb.storage();
+export const auth = fb.auth();
+export default fb;
