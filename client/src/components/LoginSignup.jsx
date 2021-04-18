f6dbcd659860f86c1ae99e35c157955411674ed6
liamohkay <liam.olsonkenny@gmail.com>
2021-04-18 16:40:44 -0700

    Added redirect from login to link

--- client/src/components/LoginSignup.jsx
+++ client/src/components/LoginSignup.jsx
@@ -1,26 +1,28 @@
 // Libraries + dependencies
 import axios from 'axios';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext.js';
 import useText from '../hooks/useText.js';
 
 const Login = () => {
   const { login, signup } = useAuth();
-  const [text, setText] = useText({
-    email: '',
-    password: ''
-  });
+  const history = useHistory();
+  const [text, setText] = useText({ email: '', password: '' });
 
   // onClick events for login + signup buttons
   const handleLogin = () => {
     login(text.email, text.password)
-      .then(() => console.log('Signin successful'))
+      .then(() => history.push('/link'))
       .catch(err => console.log(err))
   }
 
   const handleSignup = () => {
     signup(text.email, text.password)
-      .then(resp => alert(`Account created for ${resp.user.email}`))
+      .then(resp => {
+        alert(`Account created for ${resp.user.email}`);
+        history.push('/link');
+      })
       .catch(err => console.log(err))
   }
 
