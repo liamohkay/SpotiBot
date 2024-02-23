ffccdd054da99a7b7d759d589446c7ec082ed23d
Liam OK <liamohkay@gmail.com>
2024-02-23 00:16:48 -0500

    React router new version acting up

--- client/src/components/LinkSpotify.jsx
+++ client/src/components/LinkSpotify.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 // Prompts users to link their spotify account + initiates oauth
 export default function LinkSpotify() {
   return (
-    <div className="container flex">
-      <div id="link-spotify-card" className="flex">
-        <h1>SpotiBot</h1>
+    <div className="flex border-bg">
+      <div className="nav-bg">
+
+      </div>
+      <main className="main-bg">
         <a href="/login">
           <button id="login-btn">Link Your Spotify Account</button>
         </a>
-      </div>
+      </main>
     </div>
   );
 }
