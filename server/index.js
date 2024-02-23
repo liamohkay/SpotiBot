065151086f4c7c4aaa190c9ec514fccad0b92bd3
Liam OK <liamohkay@gmail.com>
2024-02-23 00:26:49 -0500

    Added missing env vars to server

--- server/index.js
+++ server/index.js
@@ -93,7 +93,7 @@ app.get('/refresh_token', (req, res) => {
   let authOptions = {
     json: true,
     url: 'https://accounts.spotify.com/api/token',
-    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    headers: { 'Authorization': 'Basic ' + (new Buffer(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')) },
     form: {
       grant_type: 'refresh_token',
       refresh_token: refresh_token
